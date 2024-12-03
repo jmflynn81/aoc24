a428f033df9314a52ecb02733a3b271b749790c5
Jonathan Flynn <Jonathan.Flynn@experian.com>
2024-12-03 10:38:24 +0000

    Day3

--- src/03/1.py
+++ src/03/1.py
@@ -1,2 +1,9 @@
+import re
+
+
 def main(data):
-  return 1
+  total = 0
+  for line in data:
+    matches = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', line)
+    total += sum(int(match[0])*int(match[1]) for match in matches)
+  return total
--- src/03/2.py
+++ src/03/2.py
@@ -1,2 +1,16 @@
+import re
+
+
 def main(data):
-  return 2
+  total = 0
+  do = True
+  for line in data:
+    matches = re.findall(r"(?:(do)\(\)|(don)'t\(\)|mul\((\d{1,3}),(\d{1,3})\))", line)
+    for match in matches:
+      if match[1]:
+        do = False
+      elif match[0]:
+        do = True
+      elif do:
+        total += int(match[2])*int(match[3])
+  return total
